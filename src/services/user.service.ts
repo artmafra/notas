813b9ac8e66df98b5b4cc2284ca5394c96e054a6
Arthur Mafra <artmafra@gmail.com>
2025-10-22 14:19:22 -0300

    encrypt password system added

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,13 +1,24 @@
 import { CreateUserSchema, UpdateUserSchema } from "@/db/schemas";
 import { storage } from "@/storage";
+import bcrypt from "bcrypt";
 
 export class UserService {
   async getAllUsers() {
     return storage.user.getAllUsers();
   }
   async createUser(data: CreateUserSchema) {
-    return storage.user.createUser(data);
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    return storage.user.createUser({
+      ...data,
+      password: hashedPassword,
+    });
   }
+
+  async verifyPassword(plainPassword: string, hashedPassword: string) {
+    return await bcrypt.compare(plainPassword, hashedPassword);
+  }
+
   async updateUser(id: number, data: UpdateUserSchema) {
     return storage.user.updateUser(id, data);
   }
